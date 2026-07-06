f97795274a8a398e0902e0c621936a0ae8d345b1
Dishamathur0625 <dishamathur23.set@modyuniversity.ac.in>
2026-07-06 18:17:47 +0530

    Fix: Lazy initialize database pool to prevent Render startup crashes

--- database/db_connection.py
+++ database/db_connection.py
@@ -1,17 +1,20 @@
 import mysql.connector.pooling
 from config import Config
 
-# Initialize connection pool globally
-db_pool = mysql.connector.pooling.MySQLConnectionPool(
-    pool_name=Config.MYSQL_POOL_NAME,
-    pool_size=Config.MYSQL_POOL_SIZE,
-    pool_reset_session=True,
-    host=Config.MYSQL_HOST,
-    user=Config.MYSQL_USER,
-    password=Config.MYSQL_PASSWORD,
-    database=Config.MYSQL_DATABASE
-)
+# Initialize connection pool globally as None, to be populated lazily
+db_pool = None
 
 def get_db_connection():
-    """Returns a connection from the global MySQL connection pool."""
+    """Returns a connection from the global MySQL connection pool, initializing it lazily."""
+    global db_pool
+    if db_pool is None:
+        db_pool = mysql.connector.pooling.MySQLConnectionPool(
+            pool_name=Config.MYSQL_POOL_NAME,
+            pool_size=Config.MYSQL_POOL_SIZE,
+            pool_reset_session=True,
+            host=Config.MYSQL_HOST,
+            user=Config.MYSQL_USER,
+            password=Config.MYSQL_PASSWORD,
+            database=Config.MYSQL_DATABASE
+        )
     return db_pool.get_connection()
